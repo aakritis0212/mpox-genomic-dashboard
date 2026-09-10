2aec4d1aa4e742d7bef021f9538f74d4b0862db2
AAkriti sharma <121090338+aakritis0212@users.noreply.github.com>
2026-09-10 17:12:35 +0530

    app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
+import genomic
 # 1. Page Configuration
 st.set_page_config(page_title="Mpox Epi Dashboard", layout="wide")
 st.title("🦠 Mpox Genomic Epidemiology Dashboard")
